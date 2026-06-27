38ca712c09e83f783476f5ea97524d521b050171
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-06-27 20:16:28 +0530

    Time: 18 ms (79.59%), Space: 77.5 MB (62.27%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -5,18 +5,28 @@ public:
 
         int left = 0;
 
-        map<int,int>mp;
+        deque<int>maxDQ;
+        deque<int>minDQ;
 
         for(int right = 0; right < n; right++) {
-            mp[nums[right]]++;
+            while(!maxDQ.empty() && maxDQ.back() < nums[right] ) {
+                maxDQ.pop_back();
+            }
 
-            int currMax = mp.rbegin()->first;
-            int currMin = mp.begin()->first;
+            maxDQ.push_back(nums[right]);
 
-            if(currMax - currMin > limit) {
-                mp[nums[left]]--;
-                if(mp[nums[left]] == 0) {
-                    mp.erase(nums[left]);
+            while(!minDQ.empty() && minDQ.back() > nums[right]) {
+                minDQ.pop_back();
+            }
+
+            minDQ.push_back(nums[right]);
+
+            if(maxDQ.front() - minDQ.front() > limit) {
+                if(maxDQ.front() == nums[left]) {
+                    maxDQ.pop_front();
+                }
+                if(minDQ.front() == nums[left]) {
+                    minDQ.pop_front();
                 }
                 left++;
             }
