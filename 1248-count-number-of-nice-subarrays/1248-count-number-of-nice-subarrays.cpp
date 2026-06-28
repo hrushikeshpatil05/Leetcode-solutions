e6e8c1c579b1704d1dafdf65bdabcadc462e8b70
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-06-28 16:44:04 +0530

    Time: 71 ms (17.25%), Space: 94.4 MB (9%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int k) {
+    // int solve(vector<int>& nums,int k) {
+    //     int n = nums.size();
+
+    //     int left = 0;
+    //     int ctOfArrays = 0;
+    //     int oddCt = 0;
+
+    //     for(int right = 0; right < n; right++) {
+    //         if(nums[right]%2 != 0) {
+    //             oddCt++;
+    //         }
+
+    //         while(oddCt > k) {
+    //             if(nums[left]%2 != 0) {
+    //                 oddCt--;
+    //             }
+    //             left++;
+    //         }
+
+    //         ctOfArrays += (right - left + 1);
+    //     }
+    //     return ctOfArrays;
+    // }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        
         int n = nums.size();
 
-        int left = 0;
-        int ctOfArrays = 0;
-        int oddCt = 0;
-
-        for(int right = 0; right < n; right++) {
-            if(nums[right]%2 != 0) {
-                oddCt++;
+        unordered_map<int,int>mp;
+        mp[0] = 1;
+        int currCt = 0;
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            if(nums[i]%2 != 0) {
+                currCt++;
             }
 
-            while(oddCt > k) {
-                if(nums[left]%2 != 0) {
-                    oddCt--;
-                }
-                left++;
-            }
+            int requiredCt = currCt - k;
 
-            ctOfArrays += (right - left + 1);
+            if(mp.find(requiredCt) != mp.end()) {
+                ans += mp[requiredCt];
+            }
+            
+            mp[currCt]++;
         }
-        return ctOfArrays;
-    }
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        return solve(nums,k) - solve(nums,k-1);
+        return ans;
 
     }
 };
