efd8903270f52e980607133da4cf19d5a3b34c98
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-07-21 22:54:34 +0530

    Time: 55 ms (65.77%), Space: 19.8 MB (90.93%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -5,10 +5,7 @@ public:
 
         queue<string>q;
         q.push(beginWord);
-        unordered_map<string,int>mp;
-        for(auto x:wordList) {
-            mp[x]++;
-        }
+        unordered_set<string>mp(wordList.begin(),wordList.end());
         
         if(mp.find(beginWord) != mp.end()) {
             mp.erase(beginWord);
@@ -22,24 +19,26 @@ public:
 
         while(!q.empty()) {
             int size = q.size();
-            level++;
             while(size--) {
                 auto top = q.front();
                 q.pop();
+                if(top == endWord) {
+                    return level;
+                }
                 for(int i=0;i<top.size();i++) {
-                    string curr = top;
+                    char originalChar = top[i];
                     for(char c = 'a'; c <= 'z'; c++) {
-                        curr[i] = c;
-                        if(curr == endWord) {
-                            return level;
-                        }
-                        if(mp.find(curr) != mp.end()) {
-                            q.push(curr);
-                            mp.erase(curr);
+                        if(c == originalChar) continue;
+                        top[i] = c;
+                        if(mp.find(top) != mp.end()) {
+                            q.push(top);
+                            mp.erase(top);
                         }
                     }
+                    top[i] = originalChar;
                 }
             }
+            level++;
         }
         return 0;
     }
