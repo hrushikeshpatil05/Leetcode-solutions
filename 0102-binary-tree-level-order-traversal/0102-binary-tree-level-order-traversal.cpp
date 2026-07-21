694ff64d9aefd273a2887b817b8830e22d3e5076
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-07-21 08:25:15 +0530

    Time: 0 ms (100%), Space: 17.2 MB (16.14%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -17,21 +17,21 @@ public:
             return ans;
         }
 
-        queue<TreeNode*> q;
+        queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()) {
-            vector<int>temp;
             int size = q.size();
+            vector<int>temp;
             while(size--) {
-                TreeNode* top = q.front();
+                auto top = q.front();
                 q.pop();
-                temp.push_back(top->val);
                 if(top->left) {
                     q.push(top->left);
                 }
                 if(top->right) {
                     q.push(top->right);
                 }
+                temp.push_back(top->val);
             }
             ans.push_back(temp);
         }
