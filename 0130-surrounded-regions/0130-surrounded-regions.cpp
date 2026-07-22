2af0fe0bcc4e1dadfe0fb6374ded14ec64136405
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-07-22 08:16:58 +0530

    Time: 0 ms (100%), Space: 14 MB (81.95%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -43,12 +43,7 @@ public:
                 if(board[i][j] == 'O') {
                     board[i][j] = 'X';
                 }
-            }
-        }
-
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                if(board[i][j] == 'z') {
+                else if(board[i][j] == 'z') {
                     board[i][j] = 'O';
                 }
             }
