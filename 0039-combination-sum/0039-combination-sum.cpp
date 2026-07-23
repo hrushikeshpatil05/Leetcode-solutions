4718c146fe46b976270cd2977b48119695b7787c
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-07-23 08:37:20 +0530

    Time: 0 ms (100%), Space: 13.9 MB (93.9%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -2,35 +2,26 @@ class Solution {
 public:
     int n;
 
-    void solve(int i, int target, int sum, vector<int>& candidates,
-               set<vector<int>>& s, vector<int>& temp) {
-        if (target == sum) {
-            s.insert(temp);
+    void solve(int i, int target, vector<int>& candidates,
+               vector<vector<int>>& ans, vector<int>& temp) {
+        if (target == 0) {
+            ans.push_back(temp);
             return;
         }
-        if (i >= n) {
+        if (i >= n || target < 0) {
             return;
         }
-        if (sum + candidates[i] <= target) {
-            sum += candidates[i];
-            temp.push_back(candidates[i]);
-            solve(i, target, sum, candidates, s, temp);
-            solve(i + 1, target, sum, candidates, s, temp);
-            temp.pop_back();
-            sum -= candidates[i];
-        }
-        solve(i + 1, target, sum, candidates, s, temp);
+        temp.push_back(candidates[i]);
+        solve(i, target - candidates[i], candidates, ans, temp);
+        temp.pop_back();
+        solve(i + 1, target, candidates, ans, temp);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         n = candidates.size();
         vector<int> temp;
-        set<vector<int>> s;
-        solve(0, target, 0, candidates, s, temp);
         vector<vector<int>> ans;
-        for (auto x : s) {
-            ans.push_back(x);
-        }
+        solve(0, target, candidates, ans, temp);
         return ans;
     }
 };
