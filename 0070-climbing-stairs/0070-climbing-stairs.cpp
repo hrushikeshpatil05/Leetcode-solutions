40aaa88665c296a8f7ec2386e00e6a84bd032b4a
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-07-24 22:11:23 +0530

    Time: 0 ms (100%), Space: 8.5 MB (38.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -15,7 +15,16 @@ public:
         return dp[step] = takeOneStep + takeTwoSteps;
     }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(0,n,dp);
+        if(n <= 2) {
+            return n;
+        }
+        vector<int>dp(n+1,0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+        // return solve(0,n,dp);
     }
 };
