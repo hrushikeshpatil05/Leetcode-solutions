2aaccdda49703e3cfb6c7f415f9d4fa898bb4299
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-07-24 09:05:49 +0530

    Time: 170 ms (95.3%), Space: 10.4 MB (81.71%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     int m,n;
-    int dir[4][2] = {{-1,0},{0,-1},{1,0},{0,1}};
-    bool solve(int i, int j, vector<vector<char>>& board, string& word, int idx) {
+    int dir[4][2] = {{-1,0},{0,-1},{0,1},{1,0}};
+
+    bool solve(int i, int j, int idx, vector<vector<char>>& board, string& word) {
         if(idx == word.size()) {
             return true;
         }
-        auto temp = board[i][j];
+        char originalChar = board[i][j];
         board[i][j] = '#';
+        bool ans = false;
         for(int k=0;k<4;k++) {
             int nr = i + dir[k][0];
             int nc = j + dir[k][1];
             if(nr >= 0 && nc >= 0 && nr < m && nc < n && board[nr][nc] == word[idx]) {
-                if(solve(nr,nc,board,word,idx+1)) {
-                    return true;
-                }
+                ans = ans || solve(nr,nc,idx+1,board,word);
             }
         }
-        board[i][j] = temp;
-        return false;
+        board[i][j] = originalChar;
+        return ans;
     }
     bool exist(vector<vector<char>>& board, string word) {
         m = board.size(), n = board[0].size();
-
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                if(board[i][j] == word[0] && solve(i,j,board,word,1)) {
+                if(board[i][j] == word[0] && solve(i,j,1,board,word)) {
                     return true;
                 }
             }
