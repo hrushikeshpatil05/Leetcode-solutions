5ea0b031936568163ed3142abf6115aa027eea17
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-07-24 22:28:33 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -14,7 +14,22 @@ public:
     }
     int rob(vector<int>& nums) {
         n = nums.size();
-        vector<int>dp(n+1,-1);
-        return solve(0,nums,dp);
+        if(n == 0) {
+            return 0;
+        }
+        if(n == 1) {
+            return nums[0];
+        }
+        vector<int>dp(n,0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[1],dp[0]);
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = max(nums[i] + dp[i-2],dp[i-1]);
+        }
+
+        return dp[n-1];
+
+        // return solve(0,nums,dp);
     }
 };
