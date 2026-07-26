96b8a1a62d75b15a23d6190082db45cdd288f1b0
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-07-26 12:30:18 +0530

    Time: 4 ms (81.14%), Space: 10.4 MB (96.98%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -28,30 +28,30 @@ public:
     // }
     int minDistance(string word1, string word2) {
         m = word1.size(), n = word2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 1e8));
+        // vector<vector<int>> dp(m + 1, vector<int>(n + 1, 1e8));
 
-        for (int i = 0; i <= m; i++) {
-            dp[i][n] = m - i;
-        }
+        vector<int> currRow(n + 1, 1e8), nextRow(n + 1, 1e8);
+
         for (int j = 0; j <= n; j++) {
-            dp[m][j] = n - j;
+            nextRow[j] = n - j;
         }
 
         for (int i = m - 1; i >= 0; i--) {
+            currRow[n] = m - i;
             for (int j = n - 1; j >= 0; j--) {
                 int take = 1e8;
                 int nottake = 1e8;
                 if (word1[i] == word2[j]) {
-                    take = dp[i + 1][j + 1];
+                    currRow[j] = nextRow[j + 1];
                 } else {
-                    int insertChar = 1 + dp[i][j + 1];
-                    int deleteChar = 1 + dp[i + 1][j];
-                    int replaceChar = 1 + dp[i + 1][j + 1];
-                    nottake = min({insertChar, deleteChar, replaceChar});
+                    int insertChar = 1 + currRow[j + 1];
+                    int deleteChar = 1 + nextRow[j];
+                    int replaceChar = 1 + nextRow[j + 1];
+                    currRow[j] = min({insertChar, deleteChar, replaceChar});
                 }
-                dp[i][j] = min(take, nottake);
             }
+            nextRow = currRow;
         }
-        return dp[0][0];
+        return nextRow[0];
     }
 };
