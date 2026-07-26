b3b4b24e11b260898b24c93fa95c250879021576
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-07-26 15:57:19 +0530

    Time: 0 ms (100%), Space: 9.4 MB (30.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-
-    int solve(int i, int j,int m, int n,vector<vector<int>>& dp) {
-        if(i == m-1 && j == n-1) {
-            return 1;
-        }
-        if(i >= m || j >= n) {
-            return 0;
-        }
-
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int goDown = solve(i+1,j,m,n,dp);
-        int goRight = solve(i,j+1,m,n,dp);
-
-        return dp[i][j] = goDown+ goRight;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return solve(0,0,m,n,dp);
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][n - 1] = 1;
+        }
+
+        for (int j = 0; j <= n; j++) {
+            dp[m - 1][j] = 1;
+        }
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                int goDown = dp[i + 1][j];
+                int goRight = dp[i][j + 1];
+                dp[i][j] = goDown + goRight;
+            }
+        }
+        return dp[0][0];
     }
 };
