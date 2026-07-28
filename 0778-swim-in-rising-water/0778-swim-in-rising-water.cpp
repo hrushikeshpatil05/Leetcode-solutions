c01e12082a0ea93e65d6e938ed8ae348f7889607
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-07-28 22:35:36 +0530

    Time: 8 ms (49.32%), Space: 12.8 MB (75.63%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -8,7 +8,8 @@ public:
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
 
         pq.push({grid[0][0],{0,0}});
-
+        cost[0][0] = grid[0][0];
+ 
         int dir[4][2] = {{-1,0},{0,-1},{1,0},{0,1}};
 
         while(!pq.empty()) {
