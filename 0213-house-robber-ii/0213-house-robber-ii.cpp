1f7026d9e13c5aaa6b913c5694ba31c6676c9186
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-07-31 10:28:19 +0530

    Time: 0 ms (100%), Space: 10.3 MB (74.57%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     int n;
-    int solve(int i,vector<int>& nums,int prev,int isFirstTaken,vector<vector<vector<int>>>& dp) {
-        if(i>=n) {
-            return 0;
-        }
-        if(i == n-1 && isFirstTaken) {
-            return 0;
-        }
-        if(dp[i][prev+1][isFirstTaken] != -1) {
-            return dp[i][prev+1][isFirstTaken];
-        }
-        int nottake = solve(i+1,nums, prev,isFirstTaken,dp);
-        int take = 0;
-        if(prev == -1 || prev < i - 1) {
-            if(i == 0) {
-                isFirstTaken = 1;
-            }
-            take = nums[i] + solve(i+1,nums,i,isFirstTaken,dp);
-        }
-        return dp[i][prev+1][isFirstTaken] = max(take,nottake);
-    }
     int rob(vector<int>& nums) {
         n = nums.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1,vector<int>(2,-1)));
-        return solve(0,nums,-1,0,dp);
+        if(n == 1) {
+            return nums[0];
+        }
+        if(n == 2) {
+            return max(nums[0],nums[1]);
+        }
+
+        int prev1 = nums[0];
+        int prev2 = 0;
+
+        for(int i=1;i<n-1;i++) {
+            int curr = nums[i] + prev2;
+            prev2 = prev1;
+            prev1 = max(prev2,curr);
+        }
+
+        int prev3 = nums[1];
+        int prev4 = 0;
+
+        for(int i=2;i<n;i++) {
+            int curr = nums[i] + prev4;
+            prev4 = prev3;
+            prev3 = max(prev4,curr);
+        }
+        return max(prev1,prev3);
     }
 };
