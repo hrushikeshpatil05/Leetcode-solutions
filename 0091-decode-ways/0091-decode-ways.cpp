d6b39f32e1dd44fcc9821b714455f73fa146c613
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-07-31 12:31:45 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.11%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,28 +1,46 @@
 class Solution {
 public:
     int n;
-    int solve(int i,string& s,vector<int>& dp) {
-        if(i>=n) {
-            return 1;
-        }
+    // int solve(int i,string& s,vector<int>& dp) {
+    //     if(i>=n) {
+    //         return 1;
+    //     }
 
-        if(s[i] == '0') return 0;
+    //     if(s[i] == '0') return 0;
 
-        if(dp[i] != -1) {
-            return dp[i];
-        }
+    //     if(dp[i] != -1) {
+    //         return dp[i];
+    //     }
 
-        int ways = solve(i+1,s,dp);
+    //     int ways = solve(i+1,s,dp);
 
-        if(i+1 < n && (s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))) {
-            ways += solve(i+2,s,dp);
-        }
+    //     if(i+1 < n && (s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))) {
+    //         ways += solve(i+2,s,dp);
+    //     }
 
-        return dp[i] = ways;
-    }
+    //     return dp[i] = ways;
+    // }
     int numDecodings(string s) {
         n = s.size();
-        vector<int>dp(n+1,-1);
-        return solve(0,s,dp);
+        vector<int> dp(n + 1, 0);
+        if (s[0] == '0') {
+            return 0;
+        }
+        dp[n] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '0') {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i+1];
+                if (i + 1 < n &&
+                    (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+
+        return dp[0];
+        // return solve(0,s,dp);
     }
 };
