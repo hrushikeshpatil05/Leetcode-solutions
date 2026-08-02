72b75a83aa94ca3735571de32555292f074a9779
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-08-02 19:22:48 +0530

    Time: 49 ms (85.65%), Space: 14.8 MB (66%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int size;
-    int dp[601][101][101];
-    int solve(int i,vector<vector<int>>& nums,int m,int n) {
-        if(i>=size) {
-            return 0;
-        }
+    // int dp[601][101][101];
+    // int solve(int i,vector<vector<int>>& nums,int m,int n) {
+    //     if(i>=size) {
+    //         return 0;
+    //     }
 
-        if(dp[i][m][n] != -1) {
-            return dp[i][m][n];
-        }
+    //     if(dp[i][m][n] != -1) {
+    //         return dp[i][m][n];
+    //     }
 
-        int nottake = solve(i+1,nums,m,n);
-        int take = 0;
-        if(nums[i][0] <= m && nums[i][1] <= n) {
-            take = 1 + solve(i+1,nums,m-nums[i][0],n-nums[i][1]);
-        }
-        return dp[i][m][n] = max(take,nottake);
-    }
+    //     int nottake = solve(i+1,nums,m,n);
+    //     int take = 0;
+    //     if(nums[i][0] <= m && nums[i][1] <= n) {
+    //         take = 1 + solve(i+1,nums,m-nums[i][0],n-nums[i][1]);
+    //     }
+    //     return dp[i][m][n] = max(take,nottake);
+    // }
     int findMaxForm(vector<string>& strs, int m, int n) {
         size = strs.size();
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
         vector<vector<int>>nums;
         for(int i=0;i<size;i++) {
             int ct0 = 0,ct1 = 0;
@@ -30,6 +30,19 @@ public:
             }
             nums.push_back({ct0,ct1});
         }
-        return solve(0,nums,m,n);
+
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+
+
+        for(auto num : nums) {
+            int ct0 = num[0], ct1 = num[1];
+            for(int i=m;i>=ct0;i--) {
+                for(int j=n;j>=ct1;j--) {
+                    dp[i][j] = max(dp[i][j],1+dp[i-ct0][j-ct1]);
+                }
+            }
+        }
+        return dp[m][n];
+        // return solve(0,nums,m,n);
     }
 };
