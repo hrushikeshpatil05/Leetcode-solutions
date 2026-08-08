8d8ed10534378defa50bbc81542c78d7074111bf
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-08-08 12:23:45 +0530

    Time: 0 ms (100%), Space: 11.5 MB (90.12%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     int m, n;
-    // int dp[501][501];
     int minDistance(string word1, string word2) {
         m = word1.size(), n = word2.size();
-        // memset(dp, 0, sizeof(dp));
 
         vector<int>next(n+1,0);
         vector<int>curr(n+1,0);
-
-        // for(int i=0;i<=m;i++) {
-        //     dp[i][n] = m-i;
-        // }
 
         for(int j=0;j<=n;j++) {
             next[j] = n-j;
         }
 
         for (int i = m - 1; i >= 0; i--) {
-            curr[n] = m - i;
+            int prevDiag = next[n];
+            next[n] = m - i;
             for (int j = n - 1; j >= 0; j--) {
-                int take = 1e8;
+                int temp = next[j];
                 if (word1[i] == word2[j]) {
-                    take = next[j + 1];
+                    next[j] = prevDiag;
                 }
-                int deleteChar1 = 1 + next[j];
-                int deleteChar2 = 1 + curr[j + 1];
-
-                curr[j] = min({take, deleteChar1, deleteChar2});
+                else {
+                    next[j] = 1 + min(next[j],next[j+1]);
+                }
+                prevDiag = temp;
             }
-            next = curr;
         }
         return next[0];
     }
