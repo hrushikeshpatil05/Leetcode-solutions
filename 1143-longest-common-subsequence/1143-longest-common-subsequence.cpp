269ebf12ca175436cefff1179d4aac2b47df5213
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-08-08 11:33:28 +0530

    Time: 48 ms (9.13%), Space: 12.8 MB (88.39%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int m,n;
+    int m, n;
     int dp[1001][1001];
-    int solve(int i,int j,string& text1, string& text2) {
-        if(i>=m || j>=n) {
-            return 0;
-        }
-
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int nottakeFirst = solve(i+1,j,text1,text2);
-        int nottakeSecond = solve(i,j+1,text1,text2);
-        int take = 0;
-        if(text1[i] == text2[j]) {
-            take = 1 + solve(i+1,j+1,text1,text2);
-        }
-        return dp[i][j] = max({nottakeFirst,nottakeSecond,take});
-    }
     int longestCommonSubsequence(string text1, string text2) {
         m = text1.size(), n = text2.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,text1,text2);
+        memset(dp, 0, sizeof(dp));
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int nottakeFirst = dp[i + 1][ j];
+                int nottakeSecond = dp[i][j + 1];
+                int take = 0;
+                if (text1[i] == text2[j]) {
+                    take = 1 + dp[i + 1][j + 1];
+                }
+                dp[i][j] = max({nottakeFirst, nottakeSecond, take});
+            }
+        }
+        return dp[0][0];
     }
 };
