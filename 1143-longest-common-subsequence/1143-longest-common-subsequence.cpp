7e058b7455e49643c89fec3c85c7972031021431
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-08-08 11:38:03 +0530

    Time: 39 ms (32.71%), Space: 9 MB (99.39%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -5,11 +5,11 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         m = text1.size(), n = text2.size();
         // memset(dp, 0, sizeof(dp));
-        vector<int>next(n+1,0);
-        vector<int>curr(n+1,0);
+        vector<int> next(n + 1, 0);
+        vector<int> curr(n + 1, 0);
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                int nottakeFirst = curr[j+1];
+                int nottakeFirst = curr[j + 1];
                 int nottakeSecond = next[j];
                 int take = 0;
                 if (text1[i] == text2[j]) {
