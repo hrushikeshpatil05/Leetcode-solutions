3d2c74fa3a95e3a0ed84a80f75afd36763915b0f
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-08-08 12:14:21 +0530

    Time: 10 ms (69.01%), Space: 11.4 MB (93.55%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,57 +1,36 @@
 class Solution {
 public:
     int m, n;
-    int dp[501][501];
-    int solve(int i, int j, string& word1, string& word2) {
-        if (i > m || j > n) {
-            return 1e8;
-        }
-
-        if (i == m) {
-            return n - j;
-        }
-
-        if (j == n) {
-            return m - i;
-        }
-
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int take = 1e8;
-        if (word1[i] == word2[j]) {
-            take = solve(i + 1, j + 1, word1, word2);
-        }
-        int deleteChar1 = 1 + solve(i + 1, j, word1, word2);
-        int deleteChar2 = 1 + solve(i, j + 1, word1, word2);
-
-        return dp[i][j] = min({take, deleteChar1, deleteChar2});
-    }
+    // int dp[501][501];
     int minDistance(string word1, string word2) {
         m = word1.size(), n = word2.size();
-        memset(dp, 0, sizeof(dp));
+        // memset(dp, 0, sizeof(dp));
 
-        for(int i=0;i<=m;i++) {
-            dp[i][n] = m-i;
-        }
+        vector<int>next(n+1,0);
+        vector<int>curr(n+1,0);
+
+        // for(int i=0;i<=m;i++) {
+        //     dp[i][n] = m-i;
+        // }
 
         for(int j=0;j<=n;j++) {
-            dp[m][j] = n-j;
+            next[j] = n-j;
         }
 
         for (int i = m - 1; i >= 0; i--) {
+            curr[n] = m - i;
             for (int j = n - 1; j >= 0; j--) {
                 int take = 1e8;
                 if (word1[i] == word2[j]) {
-                    take = dp[i + 1][j + 1];
+                    take = next[j + 1];
                 }
-                int deleteChar1 = 1 + dp[i + 1][j];
-                int deleteChar2 = 1 + dp[i][j + 1];
+                int deleteChar1 = 1 + next[j];
+                int deleteChar2 = 1 + curr[j + 1];
 
-                dp[i][j] = min({take, deleteChar1, deleteChar2});
+                curr[j] = min({take, deleteChar1, deleteChar2});
             }
+            next = curr;
         }
-        return dp[0][0];
+        return next[0];
     }
 };
