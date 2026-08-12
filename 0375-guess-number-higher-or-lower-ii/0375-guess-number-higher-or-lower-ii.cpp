122ea88400ee74c372d8fa471c1aafc3e1c1090b
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-08-12 18:41:10 +0530

    Time: 15 ms (97.9%), Space: 7.8 MB (99%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int dp[201][201];
-    int solve(int i,int j) {
-        if(i>=j) {
-            return 0;
-        }
-
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        int minCost = 1e8;
-        for(int k=i;k<=j;k++) {
-            int goRight = solve(i,k-1);
-            int goLeft = solve(k+1,j);
-
-            minCost = min(minCost,k + max(goRight,goLeft));
-        }
-        return dp[i][j] = minCost;
-    }
     int getMoneyAmount(int n) {
-        memset(dp,-1,sizeof(dp));
-       return solve(1,n);
+        memset(dp,0,sizeof(dp));
+
+        for(int i=n;i>=1;i--) {
+            for(int j=i;j<=n;j++) {
+                dp[i][j] = INT_MAX;
+                if(i == j) {
+                    dp[i][j] = 0;
+                }
+                for(int k=i;k<=j;k++) {
+                    dp[i][j] = min(dp[i][j], k + max(dp[i][k-1],dp[k+1][j]));
+                }
+            }
+        }
+       return dp[1][n];
     }
 };
